1f572ed52ac80fc6419592b0648ee2b4e7b06b64
rushikesh <rrkundkar777@gmail.com>
2026-07-11 19:16:30 +0530

    implemented starter array functions

--- examples/array.c
+++ examples/array.c
@@ -1,6 +1,22 @@
 #include "librxdsa.h"
 
 int main() {
-    array_print();
+    int retcode;
+    retcode = array_hello();
+    printf("array_hello %d\n", retcode);
+    
+    int* arr;
+    retcode = array_init(&arr, 11);
+    printf("array_init %d\n", retcode);
+
+    retcode = array_set_at(&arr, 5, 456, 11);
+    printf("array_set_at %d\n", retcode);
+
+    retcode = array_get_at(&arr, 5, 11);
+    printf("array_get_at %d\n", retcode);
+
+
+
+
     return 0;
 }
--- include/array.h
+++ include/array.h
@@ -1,8 +1,19 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void array_print();
+#define INDEX_OUT_OF_BOUNDS -1
+#define ARRAY_POINTER_NULL -1
+#define SUCCESS 0
+
+int array_hello();
+int array_init(int** array_pointer, int array_size);
+int array_clear(int **array_pointer, int array_size);
+int array_resize(int **array_pointer, int new_size);
+int array_set_at(int **array_pointer, int index, int value, int array_size);
+int array_get_at(int** array_pointer, int index, int array_size);
 
 #endif // ARRAY_H
