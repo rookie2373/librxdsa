f419049e74e3d1c45e1fde4d353d54e5b2b444ee
rushikesh <rrkundkar777@gmail.com>
2026-09-01 00:13:55 +0530

    version upgrade

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,6 +1,6 @@
 #include "hello_world.h"
 
 void hello_world(){
-	printf("rxDSA 2026.1.2 (D2026.1.2, Jan 11 2026, 09:17:13) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin\n");
+	printf("rxDSA 0.0.2 (0.0.2, Sep 1 2026, 00:12:13) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin\n");
 	printf("Visit \"github.com/rookie2373\" for more information\n");
 }
