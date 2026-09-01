5db34e34bc428671c83f671bc71b669f902942dc
rushikesh <rrkundkar777@gmail.com>
2026-09-01 23:59:28 +0530

    updated tag to 0.0.3

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,6 +1,6 @@
 #include "hello_world.h"
 
 void hello_world(){
-	printf("rxDSA 0.0.2 (0.0.2, Sep 1 2026, 00:12:13) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin\n");
+	printf("rxDSA 0.0.3 (0.0.3, Sep 1 2026, 00:12:13) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin\n");
 	printf("Visit \"github.com/rookie2373\" for more information\n");
 }
